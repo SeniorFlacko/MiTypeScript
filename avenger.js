2c2eb44a58a5d5e5cb66c4915b9566545500d181
seniorflacko <cristianalemanfuentes@gmail.com>
2017-08-04 15:49:03 -0500

    Destructuracion de objetos

---
+++ avenger.js
@@ -0,0 +1,8 @@
+"use strict";
+var avenger = {
+    nombre: "Steve",
+    clave: "Capitan America",
+    poder: "Droga"
+};
+var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+console.log(nombre, clave, poder);
